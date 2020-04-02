42c9512e17c9a7e3e469c626e18a2b9378d651d3
BetoSalas2015 <roberto.salazar@upaep.mx>
2020-04-02 17:00:52 -0600

    Avance al 02/04/2020

--- Ejercicio01/app.js
+++ Ejercicio01/app.js
@@ -1,18 +1,43 @@
-const argv = require('yargs').argv;
+const argv = require('yargs')
+    .command('listar', 'Imprime en pantalla la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .command('crear', 'Crea un archivo con la tabla de multiplicar especificada', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
 
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const { crearArchivo, imprimeTabla } = require('./multiplicar/multiplicar');
+const colors = require('colors');
 
+let comando = argv._[0];
 
-let argv2 = process.argv;
+switch (comando) {
+    case 'listar':
+        imprimeTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite).then((archivo) => console.log(`Archivo creado: ${archivo}`),
+            (err) => {
+                console.log(err)
+            });
+        break;
+    default:
+        console.log('Comando no reconocido'.red);
 
-console.log(argv2);
-console.log(argv);
-
-let base = argv.base;
-
-
-
-crearArchivo(base).then((archivo) => console.log(`Archivo creado: ${archivo}`),
-    (err) => {
-        console.log(err)
-    });
+}
--- Ejercicio01/multiplicar/multiplicar.js
+++ Ejercicio01/multiplicar/multiplicar.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const colors = require('colors');
 
 crearArchivo = (base) => {
     return new Promise((resolve, reject) => {
@@ -20,6 +21,21 @@ crearArchivo = (base) => {
     });
 };
 
+imprimeTabla = (base, limite) => {
+
+    console.log('=============================='.magenta);
+    console.log(`        Tabla del ${base}`.magenta);
+
+    console.log('=============================='.magenta);
+
+
+    for (let index = 1; index <= limite; index++) {
+        console.log(`${base} * ${index} = ${base * index} `);
+    }
+}
+
+
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    imprimeTabla
 }
